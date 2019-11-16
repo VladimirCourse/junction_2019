7d52afcc54ae008e515ee106d79fa00a97119d10
shvv733kn@gmail.com <shvv733kn@gmail.com>
2019-11-17 01:59:01 +0200

    finish

--- back/app/helpers/sales_helper.rb
+++ back/app/helpers/sales_helper.rb
@@ -1,6 +1,10 @@
 class SalesHelper
 
     def self.calculate(place, position)
+        bookings = place.bookings.where('date >= DateTime.now').length
+        dist = place.distance(position) / (6 * 10000)
+        pk = sum(traffic)
+        
         
     end
 
