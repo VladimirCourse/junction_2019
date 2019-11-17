bf946285104bd4e8fb7e06030fa7835933606c26
shvv733kn@gmail.com <shvv733kn@gmail.com>
2019-11-17 08:56:06 +0200

    fix

--- back/app/helpers/sales_helper.rb
+++ back/app/helpers/sales_helper.rb
@@ -2,10 +2,15 @@ class SalesHelper
 
     def self.calculate(place, position)
         bookings = place.bookings.where('date >= DateTime.now').length
-        dist = place.distance(position) / (6 * 10000)
-        pk = sum(traffic)
-        
-        
+        places = Place.near(place)
+        exp = place.expire
+        likes = place.likes.lenght
+        disc = Place.where(discount: true).order(:d8sc_value).last
+        dist = place.distance(position) / (6 * 1000)
+        pk = sum(traffic) / dist
+        wk = sum(weather) / dist
+
+        return max(0, ((bookings - (pk + wk) * bookings) * (disc / places) * exp * likes)
     end
 
 end
